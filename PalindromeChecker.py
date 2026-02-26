ff6eb2b6056e66587a9a6dd8c26c370a25e21939
Aineen Sayyed Ismail <aineensayyed2005@gmail.com>
2026-02-26 14:51:40 +0530

    Update PalindromeChecker.py

--- PalindromeChecker.py
+++ PalindromeChecker.py
@@ -1,8 +1,3 @@
-"""PROGRAM FOR CHECKING THE GIVEN INPUT IS PALINDROME OR NOT
-PROGRAM NAME: PALINDROME CHECKER
-AUTHOR: AINEEN SAYYED
-DATE CREATED: 6/02/2026"""
-
 
 phrase = input("Enter a phrase/word/sequence:").strip().lower()
 print(f"{'Yes It is Palindrome.'if phrase == phrase[::-1] else 'Not a Palindrome.'}")
