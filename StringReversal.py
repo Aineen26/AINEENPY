bf41f9abf016d603542ff2b523357cccd6d86ad6
Aineen Sayyed Ismail <aineensayyed2005@gmail.com>
2026-02-26 14:52:36 +0530

    Update StringReversal.py

--- StringReversal.py
+++ StringReversal.py
@@ -1,14 +1,8 @@
-"""
-PROGRAM NAME: StringReversal
-AUTHOR: AINEEN SAYYED
-DATE CREATED: 24/01/2026
-"""
-# Program to reverse the string taken from user
+
 print("Welcome to StringReversal Program of Python")
 
-def reverse_string(string): #Here this function takes the input as string
-    return string[::-1]   #returns string in the reversed form
-
+def reverse_string(string): 
+    return string[::-1]   
 input_string = input("Enter your string: ")
 reversed_str =reverse_string(input_string)
 print("Here is your reversed string:",reversed_str)
