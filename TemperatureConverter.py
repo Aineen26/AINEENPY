36660aa7c9355040820d0db78fbaa09f9ecff277
Aineen Sayyed Ismail <aineensayyed2005@gmail.com>
2026-02-26 14:53:25 +0530

    Refactor temperature converter for clarity
    Removed unnecessary comments and updated input prompts.

--- TemperatureConverter.py
+++ TemperatureConverter.py
@@ -1,16 +1,12 @@
-"""PROGRAM TO CONVERT TEMPERATURE
-BETWEEN CELSIUS TO FAHRENHEIT
-PROGRAM NAME: Temperature Converter
-AUTHOR: AINEEN SAYYED
-DATE CREATED: 31/01/2026"""
+
 print("CONVERTING TEMPERATURE FROM CELSIUS TO FAHRENHEIT ")
 def temp_converter():
-    #Get the user input for value and unit
+  
     try:
         temp_value=float(input("Enter temperature in Value: "))
         unit = input("Is this in C/F?").strip().upper()
 
-        #Logic & formula
+      
         #Celsius to Fahrenheit
         if unit == "C":
             converted=(temp_value*9/5)+32
@@ -27,12 +23,6 @@ def temp_converter():
 if __name__ == "__main__":
     temp_converter()
 
-""" If we use Modern line code"""
-# temp,unit=float(input("temperature Value:")),input("Unit(C/F):").upper()
-#
-# if unit == 'C':
-#     print(f"Converted value:{temp * 9 / 5 + 32}")
-# else:
-#     print(f"({(temp -32)*5/9:.2f}{'F' if unit == 'C' else 'C'}")
+
 
 
